362358832eb6f8b3fec8dd8d825d82f3a1285eb6
xiongbk <istud.k@gmail.com>
2021-06-28 17:12:38 +0800

    swift ui 第一课

---
+++ Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by xbk on 2021/6/28.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
