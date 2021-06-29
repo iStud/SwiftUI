7590360907c76aaa760b12ec303ad69098f5cf51
xiongbk <istud.k@gmail.com>
2021-06-29 11:27:53 +0800

    创建cell和点击效果

--- Landmarks/Landmarks/Views/CircleImage.swift
+++ Landmarks/Landmarks/Views/CircleImage.swift
@@ -8,13 +8,19 @@
 import SwiftUI
 
 struct CircleImage: View {
+    
+    var image: Image
+
     var body: some View {
-        Image("turtlerock").clipShape(Circle()).overlay(Circle().stroke(Color.white,lineWidth: 4)).shadow(radius: 7)
+        image
+            .clipShape(Circle())
+            .overlay(Circle().stroke(Color.white, lineWidth: 4))
+            .shadow(radius: 7)
     }
 }
 
 struct CircleImage_Previews: PreviewProvider {
     static var previews: some View {
-        CircleImage()
+        CircleImage(image: Image("turtlerock"))
     }
 }
