b7fd8d9702313f8a1de0cab04687ab055356d005
xiongbk <istud.k@gmail.com>
2021-06-29 09:50:16 +0800

    创建模型

---
+++ Landmarks/Landmarks/Model/Landmark.swift
@@ -0,0 +1,39 @@
+//
+//  Landmark.swift
+//  Landmarks
+//
+//  Created by xbk on 2021/6/29.
+//
+
+import Foundation
+import SwiftUI
+import CoreLocation
+
+struct Landmark:Hashable,Codable {
+    
+    var id:Int
+    var name:String
+    var park:String
+    var state: String
+    var description: String
+    private var imageName:String
+    
+    var image: Image { // 计算属性
+         Image(imageName)
+     }
+    
+    private var coordinates: Coordinates
+    
+    var locationCoordinate:CLLocationCoordinate2D{
+        
+        CLLocationCoordinate2D(
+            
+            latitude: coordinates.latitude, longitude: coordinates.longitude
+        )
+    }
+
+    struct Coordinates: Hashable, Codable {
+          var latitude: Double
+          var longitude: Double
+    }
+}
