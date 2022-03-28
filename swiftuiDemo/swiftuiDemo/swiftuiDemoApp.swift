92065689c82d66f8b30121cc834d21101ffbf14d
xiongbk <istud.k@gmail.com>
2022-03-28 20:10:37 +0800

    初始化

---
+++ swiftuiDemo/swiftuiDemo/swiftuiDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftuiDemoApp.swift
+//  swiftuiDemo
+//
+//  Created by xbk on 2022/3/28.
+//
+
+import SwiftUI
+
+@main
+struct swiftuiDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
