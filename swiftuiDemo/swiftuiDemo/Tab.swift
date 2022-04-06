dbaa2ae336892d5ccbf5f8a35ca026f88b6f9e27
xiongbk <istud.k@gmail.com>
2022-04-06 10:48:26 +0800

    Tab Bar Selection

--- swiftuiDemo/swiftuiDemo/Tab.swift
+++ swiftuiDemo/swiftuiDemo/Tab.swift
@@ -11,13 +11,21 @@ struct TabItem:Identifiable {
     var id = UUID()
     var text:String
     var icon:String
+    var tab:Tab
 }
 
 var TabItems = [
-    TabItem(text:"Learn now" , icon:"house"),
-    TabItem(text:"Explore" , icon:"magnifyingglass"),
-    TabItem(text:"Notifications" , icon:"bell"),
-    TabItem(text:"Library" , icon:"rectangle.stack")
+    TabItem(text:"Learn now" , icon:"house",tab:.home),
+    TabItem(text:"Explore" , icon:"magnifyingglass",tab:.explore),
+    TabItem(text:"Notifications" , icon:"bell",tab: .notifications),
+    TabItem(text:"Library" , icon:"rectangle.stack",tab: .library)
     
 ]
+
+enum Tab:String {
+    case home
+    case explore
+    case notifications
+    case library
+}
 
