e664b0562dda04db93418d96c5deee081e5845df
xiongbk <istud.k@gmail.com>
2022-04-06 19:40:54 +0800

    注释

--- swiftuiDemo/swiftuiDemo/Tab.swift
+++ swiftuiDemo/swiftuiDemo/Tab.swift
@@ -30,6 +30,7 @@ enum Tab:String {
     case library
 }
 
+// reduce方法在Swift中非常常见，这里的用处是当有多个子View都给父View传递数据时，父View最后是只能接受一个数据，而reduce就是将子View提供的多个数据进行“操作”，降维为一个数据提供给父View使用，PreferenceKey的reduce方法包含两个参数：当前的value，和下一个要合并的值nextValue，这二个参数是子View从上到下提供的。
 struct TabPreferenceKey: PreferenceKey {
     static var defaultValue: CGFloat = 0
     static func reduce(value: inout CGFloat, nextValue: () -> CGFloat) {
--- swiftuiDemo/swiftuiDemo/TabBar.swift
+++ swiftuiDemo/swiftuiDemo/TabBar.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct TabBar: View {
     
+    // @State存储修改该属性
     @State var selectedTab:Tab = .home
     @State var color:Color = .teal
     @State var tabItemWidth:CGFloat = 0
@@ -32,101 +33,19 @@ struct TabBar: View {
             .frame(maxWidth: .infinity, maxHeight: .infinity)
             
             HStack {
-                ForEach(TabItems) { item in
-                    Button{
-                        selectedTab = item.tab
-                        color = item.color
-                    } label:{
-                        VStack(spacing: 0) {
-                            Image(systemName:item.icon)
-                                .symbolVariant(.fill)
-                                .font(.body.bold())
-                                .frame(width: 44, height: 29)
-                            Text(item.text)
-                                .font(.caption2)
-                                .lineLimit(1)
-                        }
-                        .frame(maxWidth:.infinity)
-                    }
-                    .foregroundColor(selectedTab == item.tab ? .primary:.secondary)
-                    .blendMode(selectedTab == item.tab ? .overlay:.normal)
-                    .overlay(
-                        GeometryReader{ proxy in
-                            
-                            Color.clear.preference(key: TabPreferenceKey.self, value: proxy.size.width)
-                        }
-                    )
-                    .onPreferenceChange(TabPreferenceKey.self){ value in
-                        tabItemWidth = value
-                    }
-                }
-                
+                buttons
             }
             .padding(.horizontal,8)
             .padding(.top, 14)
             .frame(height: 88, alignment: .top)
             .background(.ultraThinMaterial, in: RoundedRectangle(cornerRadius: 34, style: .continuous))
             .background(
-                HStack {
-                    
-                    if selectedTab == .library {
-                        Spacer()
-                    }
-                    if selectedTab == .explore {
-                        Spacer()
-                    }
-                    if selectedTab == .notifications {
-                        Spacer()
-                        Spacer()
-                    }
-                    Circle().fill(color).frame(width:tabItemWidth)
-                    if selectedTab == .home {
-                        Spacer()
-                    }
-                    if selectedTab == .explore {
-                        Spacer()
-                        Spacer()
-                    }
-                    if selectedTab == .notifications {
-                        Spacer()
-                        
-                    }
-                }
-                .padding(.horizontal,8)
+                
+                background
             )
             .overlay(
             
-                HStack {
-                    
-                    if selectedTab == .library {
-                        Spacer()
-                    }
-                    if selectedTab == .explore {
-                        Spacer()
-                    }
-                    if selectedTab == .notifications {
-                        Spacer()
-                        Spacer()
-                    }
-                    Rectangle()
-                        .fill(color)
-                        .frame(width: 28, height: 5)
-                        .cornerRadius(3)
-                        .frame(width:tabItemWidth)
-                        .frame(maxHeight: .infinity, alignment: .top)
-                    if selectedTab == .home {
-                        Spacer()
-                    }
-                    if selectedTab == .explore {
-                        Spacer()
-                        Spacer()
-                    }
-                    if selectedTab == .notifications {
-                        Spacer()
-                        
-                    }
-                }
-                .padding(.horizontal,8)
+                overlay
             )
             
             .strokeStyle(cornerRadius: 34)
@@ -135,11 +54,109 @@ struct TabBar: View {
         }
         
     }
+    
+    var buttons: some View {
+        ForEach(TabItems) { item in
+            Button{
+                selectedTab = item.tab
+                color = item.color
+            } label:{
+                VStack(spacing: 0) {
+                    Image(systemName:item.icon)
+                        .symbolVariant(.fill)
+                        .font(.body.bold())
+                        .frame(width: 44, height: 29)
+                    Text(item.text)
+                        .font(.caption2)
+                        .lineLimit(1)
+                }
+                .frame(maxWidth:.infinity)
+            }
+            .foregroundColor(selectedTab == item.tab ? .primary:.secondary)
+            .blendMode(selectedTab == item.tab ? .overlay:.normal)
+            .overlay(
+                
+                // GeometryReader让我们能够获取到父view提供的建议的size,保存在 proxy 中
+                //子View要想获得父View的尺寸使用GeometryReader，当父View想知道子View的尺寸时就可采用Preferences。
+                GeometryReader{ proxy in
+                    
+                    Color.clear.preference(key: TabPreferenceKey.self, value: proxy.size.width)
+                }
+            )
+            // 当父View调用onPreferenceChange方法时，会触发对应的PreferenceKey中的reduce方法
+            .onPreferenceChange(TabPreferenceKey.self){ value in
+                tabItemWidth = value
+            }
+        }
+    }
+    var background : some View{
+        
+        HStack {
+            
+            if selectedTab == .library {
+                Spacer()
+            }
+            if selectedTab == .explore {
+                Spacer()
+            }
+            if selectedTab == .notifications {
+                Spacer()
+                Spacer()
+            }
+            Circle().fill(color).frame(width:tabItemWidth)
+            if selectedTab == .home {
+                Spacer()
+            }
+            if selectedTab == .explore {
+                Spacer()
+                Spacer()
+            }
+            if selectedTab == .notifications {
+                Spacer()
+                
+            }
+        }
+        .padding(.horizontal,8)
+    }
+    
+    var overlay:some View{
+        HStack {
+            
+            if selectedTab == .library {
+                Spacer()
+            }
+            if selectedTab == .explore {
+                Spacer()
+            }
+            if selectedTab == .notifications {
+                Spacer()
+                Spacer()
+            }
+            Rectangle()
+                .fill(color)
+                .frame(width: 28, height: 5)
+                .cornerRadius(3)
+                .frame(width:tabItemWidth)
+                .frame(maxHeight: .infinity, alignment: .top)
+            if selectedTab == .home {
+                Spacer()
+            }
+            if selectedTab == .explore {
+                Spacer()
+                Spacer()
+            }
+            if selectedTab == .notifications {
+                Spacer()
+                
+            }
+        }
+        .padding(.horizontal,8)
+    }
 }
 
 struct TabBar_Previews: PreviewProvider {
     static var previews: some View {
         TabBar()
-.previewInterfaceOrientation(.landscapeLeft)
+.previewInterfaceOrientation(.portrait)
     }
 }
