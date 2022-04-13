37612ff445ad49ce26a86c77416cabaa6f6370ed
xiongbk <istud.k@gmail.com>
2022-04-13 19:48:06 +0800

    demo

---
+++ Scrumdinger/Scrumdinger/MeetingView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Scrumdinger
+//
+//  Created by xbk on 2022/4/13.
+//
+
+import SwiftUI
+
+struct MeetingView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct MeetingView_Previews: PreviewProvider {
+    static var previews: some View {
+        MeetingView()
+    }
+}
---
+++ Scrumdinger/Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by xbk on 2022/4/13.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MeetingView()
+        }
+    }
+}
