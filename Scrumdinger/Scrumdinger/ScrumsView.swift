a093d77bdcd758f15d50162fbecd738ae12eead3
xiongbk <istud.k@gmail.com>
2022-04-17 15:29:59 +0800

    Set Up Navigation

--- Scrumdinger/Scrumdinger/ScrumsView.swift
+++ Scrumdinger/Scrumdinger/ScrumsView.swift
@@ -6,9 +6,18 @@ struct ScrumsView: View {
     var body: some View {
         List {
             ForEach(scrums) { scrum in
-                CardView(scrum: scrum)
-                    .listRowBackground(scrum.theme.mainColor)
+                NavigationLink(destination:Text(scrum.title)) {
+                    CardView(scrum: scrum)
+                }
+                .listRowBackground(scrum.theme.mainColor)
             }
+        }
+        .navigationTitle("Daily Scrums")
+        .toolbar {
+            Button(action: {}) {
+                Image(systemName: "plus")
+            }
+            .accessibilityLabel("New Scrum")
         }
     }
 }
@@ -16,6 +25,8 @@ struct ScrumsView: View {
 struct ScrumsView_Previews: PreviewProvider {
     static var previews: some View {
         
-        ScrumsView(scrums:DailyScrum.sampleData)
+        NavigationView {
+            ScrumsView(scrums:DailyScrum.sampleData)
+        }
     }
 }
